784b117f2839a4c40073feaaf9c4f6a99ae0a9b1
zibb03 <64014435+zibb03@users.noreply.github.com>
2022-01-10 03:52:31 +0900

    웹 크롤링 성공
    날씨, 습도, 미세먼지 크롤링 성공. 풍향 관련한 정보만 얻으면 됨.

--- Web_Crawler.py
+++ Web_Crawler.py
@@ -18,26 +18,18 @@ print('      오늘의 주요 정보를 요약해 드리겠습니다.\n')
 print('  ○>> #오늘의 #날씨 #요약 \n')
 webpage = urllib.request.urlopen('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8')
 soup = BeautifulSoup(webpage, 'html.parser')
-# # print(soup)
-# temps = soup.find('span',"blind")
-# print(temps)
-# cast = soup.find('p',"temperature_text")
-# print(cast)
 
-a = soup.find('div', "temperature_text")
-b = a.find(a, 'span', "blind")
-print(b)
-# print('--> 서울 날씨 : ' , temps.get_text() , '℃' , cast.get_text())
+temperature = soup.find('div', "temperature_text").get_text().strip().replace('현재 온도', '')
 
-webpage = urllib.request.urlopen('https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query=%EB%8C%80%EA%B5%AC+%EB%82%A0%EC%94%A8')
-soup = BeautifulSoup(webpage, 'html.parser')
-temps = soup.find('span',"todaytemp")
-cast = soup.find('p',"cast_txt")
-print('--> 대구 날씨 : ' , temps.get_text() , '℃' , cast.get_text())
+temperature_info = soup.find('div', "temperature_info")
 
-webpage = urllib.request.urlopen('https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%B6%80%EC%82%B0+%EB%82%A0%EC%94%A8&oquery=%EB%8C%80%EA%B5%AC+%EB%82%A0%EC%94%A8&tqi=UrZy%2Bsp0YidssAyki54ssssssKC-251380')
-soup = BeautifulSoup(webpage, 'html.parser')
-temps = soup.find('span',"todaytemp")
-cast = soup.find('p',"cast_txt")
-print('--> 부산 날씨 : ' , temps.get_text() , '℃' , cast.get_text())
-print('\n')
+summary_list = soup.find('dl', attrs={'class':'summary_list'})
+humidity = summary_list.find_all('dd')[1].get_text().strip()
+wind = summary_list.find_all('dd')[2].get_text().strip()
+
+web_index = [temperature, humidity, wind]
+print(web_index)
+
+# 풍향 관련하여 고민하기
+# 풍향이 시간에 따라 있는데 이를 크롤링할 방법 생각
+# datetime 활용해서 시간만 받고 이를 통해 받기?
